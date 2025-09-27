e0036e576e1897503943c78b3ea92ccf74de3089
Dog43y <71520770+DogeDoge17@users.noreply.github.com>
2025-09-26 23:03:45 -0500

    fixed release dates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from datetime import datetime
 
 # fill out from the spotify dev dashboard
 CLIENTID=""
@@ -18,26 +19,30 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     redirect_uri="http://127.0.0.1:8888/callback",
     scope="user-library-read playlist-modify-public playlist-modify-private"
 ))
-results = sp.artist_albums(artistID, 'album,single')
 
+results = sp.artist_albums(artistID, 'album,single')
 albums = results['items']
+
 while results['next']:
     results = sp.next(results)
     albums.extend(results['items'])
 
-foundSongs = set()
+for i in range(len(albums)):
+    albums[i]['release_date'] += '2020-06-15'[len(albums[i]['release_date']):]
+albums.sort(key=lambda x: datetime.strptime(x['release_date'], "%Y-%m-%d"))
 
+foundSongs = set()
 artRes = sp.artist(artistID)
 theArt = artRes['name']
 user_id = sp.me()["id"]
 
 playlist = sp.user_playlist_create(user=user_id, name=playlistName, public=True, description= f"all songs from {theArt}")
-for album in reversed(albums):
+for album in albums:
     print(album['name'], "-", album['release_date'])
     songRes = sp.album_tracks(album['uri'])
     songs = []
     for song in songRes['items']:
-        uri = song['uri']
+        uri = song['name']
         artists = [artist["name"] for artist in song["artists"]]
         foundArtist = False
         for artist in artists:
